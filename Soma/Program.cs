767ac84c93a031032a753f833954e5eae1e6337d
josybarbosa04 <josenilda_silva123@outlook.com>
2025-01-09 18:03:44 -0300

    Adicionar arquivos de projeto.

---
+++ Soma/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soma
+{
+    class Program
+    {
+        static void Main()
+        {
+            int INDICE = 13, SOMA = 0, K = 0;
+
+            while (K < INDICE)
+            {
+                K = K + 1;
+                SOMA = SOMA + K;
+            }
+
+            Console.WriteLine($"O valor de SOMA ao final é: {SOMA}");
+            Console.ReadKey();
+        }
+    }
+
+}
